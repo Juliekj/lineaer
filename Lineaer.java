9f7c60fe41ad350ed04eee3f599cf7b334ebcd29
Juliekj <juljen2730@gmail.com>
2021-08-31 09:21:32 +0200

    Lineaer

---
+++ Lineaer.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.lang.Integer;
+
+public class Lineaer {
+static int[] array = {1,2,3,4,5,6,7,8,9,10};
+
+public static void main(String[]args){
+
+  for(int i = 0; i <array.length;i++)
+
+if(array[i]== 8){
+  System.out.println("Number:"+array[i]+"at index"+ i +"is true");
+  return;
+
+}
+  else System.out.println("Number:"+array[i]+"at index"+ i+ "is false");
+  }
+}
